244133a309bcd5e7b5b9c9ac317f24c09bc1b377
arnoldip <arnoldip@slim.eos.ubc.ca>
2013-12-20 00:22:37 +0000

    Changed month_check so that it actually converts MM/YYYY entries.
    git-svn-id: https://wave.eos.ubc.ca/SLIM/SLIM.Projects.bibtex.database@354 7fae6802-600b-0410-953c-a8125f48ba84

--- month_conversion.py
+++ month_conversion.py
@@ -4,8 +4,8 @@ import fileinput
 import re
 
 monthContainers = [
-        r'\s*month\s*?=',
-        r'\s*optmonth\s*?='
+        r'\s*month\s*=',
+        r'\s*optmonth\s*='
 ]
 
 month_full_string = [
@@ -36,6 +36,7 @@ count_int_string = 0
 count_int_string_alt = 0
 
 month_key_debug = []
+optmonth_debug = []
 for line in fileinput.input(inplace=1):
     for monthContainer in monthContainers:
         if re.match(monthContainer,line,re.I):
@@ -48,6 +49,7 @@ for line in fileinput.input(inplace=1):
                 # This basically fetches anything in between the braces.
             full_string = False
             three_string = False
+            optmonth_debug.append(month_in_container)
             for key, month in enumerate(month_full_string):
                 if month == month_in_container:
                     count_full += 1
@@ -69,8 +71,15 @@ for line in fileinput.input(inplace=1):
                         line = \
                             "  month = {{{month}}},\n".format(month=month_int_string[key])
                         month_key_debug.append((fileinput.lineno(), line,month_in_container))
+            for key, month in enumerate(month_int_string): # this is for the MM/YYYY stuff
+                if month == month_in_container:
+                    count_int_string_alt += 1
+                    line = \
+                        "  month = {{{month}}},\n".format(month=month_int_string[key])
+                    month_key_debug.append((fileinput.lineno(), line,month_in_container))
     print line,
 fileinput.close()
-print ("Found {} month instances".format(countMonth))
-print count_full, count_int_string, count_int_string_alt
-print month_key_debug
+# print ("Found {} month instances".format(countMonth))
+# print count_full, count_int_string, count_int_string_alt
+# print month_key_debug
+print optmonth_debug
